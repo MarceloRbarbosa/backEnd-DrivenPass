71c2a15c99ce39369899df11b52925e37165ccbc
unknown <marceloribeirobarbosa92@gmail.com>
2025-10-21 11:31:35 -0300

    feat(user-repository): adicionar funções CRUD para users-repository

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -1,6 +1,8 @@
 import  prisma  from "../config/database";
 
 
+
+
 async function createNewUser({name, email, password}: {name: string, email: string, password: string}) {
     const newUser =await prisma.users.create({
         data: {
@@ -13,9 +15,48 @@ async function createNewUser({name, email, password}: {name: string, email: stri
     return newUser;
 }
 
+async function findAllUser() {
+    const users = await prisma.users.findMany();
+
+    return users;
+}
+
+async function findUserByEmail(email: string) {
+    const user = await prisma.users.findUnique({
+        where: {
+            email
+        }
+    });
+
+    return user;
+}
+
+async function UpdateUser(name: string, email: string, password: string) {
+    const user = await prisma.users.update({
+        where: {
+            email
+        },
+        data: {
+            name,
+            password
+        }
+    })
+}
+
+async function DeleteUser(email: string) {
+    const user = await prisma.users.delete({
+        where: {
+            email
+        }
+    })
+}
 
 const usersRepository = {
-    createNewUser
+    createNewUser,
+    findAllUser,
+    findUserByEmail,
+    UpdateUser,
+    DeleteUser
 }
 
 export default usersRepository;
