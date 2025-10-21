ef33671a1075701be1987225420430485c33fcda
unknown <marceloribeirobarbosa92@gmail.com>
2025-10-21 12:24:51 -0300

    feat: adicionar controller para user

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { User } from "generated/prisma";
+import usersServices from "services/users.services";
+import httpStatus from "http-status";
+
+
+async function signUp(req: Request, res: Response) {
+const newUser = req.body;
+await usersServices.createNewUser(newUser);
+res.status(httpStatus).send("User created successfully");
+}
+
+async function signIn(req: Request, res: Response) {
+    const logginEmail = req.body.email
+    const logginPassword = req.body.password;
+        
+
+    const token = await usersServices.findUsers(logginEmail, logginPassword);
+    res.status(httpStatus.OK).json({  token  });
+}
+
+
+const userController = {
+    signUp,
+    signIn
+}
+
+export default userController
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,18 +1,33 @@
 import usersRepository from "repositories/users-repository";
 
 
-async function createNewUser({name, email, password}: {name: string, email: string, password: string}) {
-    const newUser = await usersRepository.createNewUser({name, email, password});
-
+async function createNewUser({name, email, password}: {name: string, email: string, password: string}) {  
     const conflict = await usersRepository.findUserByEmail(email);
     if (conflict) throw { 
         type: "conflict",
         message: "Email already exists"
     };
+    
+    const newUser = await usersRepository.createNewUser({name, email, password});
+    
     return newUser;
 }
+
+
+async function findUsers(email: string, password: string) {
+    const user = await usersRepository.findUserByEmail(email);
+
+    if(!user) throw { 
+        type: "NOT_FOUND",
+        message: "User not found"
+    };
+
+    return user;
+}
+
 const usersServices = {
-    createNewUser
+    createNewUser,
+    findUsers
 }
 
 export default usersServices;
