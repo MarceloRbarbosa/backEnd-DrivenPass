8e88274a50938d802ab3bcca37f79e724c188a46
unknown <marceloribeirobarbosa92@gmail.com>
2025-10-20 23:01:10 -0300

    feat(schemas): configurar schema de validação com joi para tabelas user e credentials

---
+++ src/schemas/credentials-schema.ts
@@ -0,0 +1,9 @@
+import joi from "joi";
+
+const credentialsSchema = joi.object({
+    title: joi.string().required(),
+    url: joi.string().required(),
+    password: joi.string().required(),
+});
+
+export default credentialsSchema;
