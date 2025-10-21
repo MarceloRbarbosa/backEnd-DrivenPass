cac7e9a63f605a2da61b66e72b94f685bb0ef796
unknown <marceloribeirobarbosa92@gmail.com>
2025-10-21 19:54:14 -0300

    feat: add rotas de credentials

--- src/routes/credentials-routers.ts
+++ src/routes/credentials-routers.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
+import credentialsController from "../controllers/credentials-controller";
+import { validateSchema } from "../middlewares/validate-schema-middleare";
+import  credentialsSchema from "../schemas/credentials-schema";
+
 
 
 const credentialsRouter = Router();
+
+credentialsRouter.post("/credentials", validateSchema(credentialsSchema), credentialsController.createCredentials);
+credentialsRouter.get("/credentials", credentialsController.getCredentials);
+credentialsRouter.put("/credentials/:id", validateSchema(credentialsSchema), credentialsController.updateCredential);
 
 
 export default credentialsRouter
