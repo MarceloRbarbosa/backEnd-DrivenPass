517821b71eacddd7f038e0eb1001eabf28dade72
unknown <marceloribeirobarbosa92@gmail.com>
2025-10-21 11:42:02 -0300

    feat(users-services): adicionar função de criar novo usuario

---
+++ src/services/users.services.ts
@@ -0,0 +1,20 @@
+import usersRepository from "repositories/users-repository";
+
+
+async function createNewUser({name, email, password}: {name: string, email: string, password: string}) {
+    const newUser = await usersRepository.createNewUser({name, email, password});
+
+    const conflict = await usersRepository.findUserByEmail(email);
+    if (conflict) throw { 
+        type: "conflict",
+        message: "Email already exists"
+    };
+    return newUser;
+}
+
+
+const usersServices = {
+    createNewUser
+}
+
+export default usersServices;
