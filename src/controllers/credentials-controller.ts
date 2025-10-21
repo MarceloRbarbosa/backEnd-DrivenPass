8d6656923a0b0e1b6f344fff7926113ad2054faa
unknown <marceloribeirobarbosa92@gmail.com>
2025-10-21 19:50:36 -0300

    feat: adicionar credentials controller

---
+++ src/controllers/credentials-controller.ts
@@ -0,0 +1,37 @@
+import { Request , Response } from "express";
+import credentialsServices from "../services/credentials-service";
+import httpStatus from "http-status";
+
+
+async function createCredentials(req: Request, res: Response) {
+    const newCredential = req.body;
+
+    await credentialsServices.createCredentials(newCredential);
+    res.status(httpStatus.CREATED).send("Credential created successfully");
+}
+
+async function getCredentials(req: Request, res: Response) {
+
+    const credentials = await credentialsServices.findAllCredentials();
+    res.status(httpStatus.OK).send(credentials);
+}
+
+async function updateCredential(req: Request, res: Response) {
+    const credentialId = Number(req.params.id);
+    const userId = Number(req.params.userId);
+    const { title, url, username, password } = req.body;
+
+    await credentialsServices.updateCredential({ 
+        id: credentialId, userId, title, url, username, password 
+    });
+    
+    return res.status(httpStatus.OK).send("Credential updated successfully");
+}
+
+const credentialsController = {
+    createCredentials,
+    getCredentials,
+    updateCredential
+}
+
+export default credentialsController;
