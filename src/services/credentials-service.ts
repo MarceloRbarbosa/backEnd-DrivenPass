05cca1868cf1fa83134bb29bf60afb26775f1ebf
unknown <marceloribeirobarbosa92@gmail.com>
2025-10-21 23:54:00 -0300

    feat: adicionar função em credentials service

--- src/services/credentials-service.ts
+++ src/services/credentials-service.ts
@@ -1,4 +1,5 @@
 import credentialsRepository from "../repositories/credentials-repository";
+import usersRepository from "../repositories/users-repository";
 
 
 async function createCredentials({title, url, username, password, userId}: {title: string, url: string, username: string, password: string, userId: number}) {
@@ -9,13 +10,19 @@ async function createCredentials({title, url, username, password, userId}: {titl
         type: "conflict",
         message: "Title already exists"
     };
+
+    const user = await usersRepository.findUserById(userId);
+    if(!user) throw {
+        type: "NOT_FOUND",
+        message: "User not found"
+    };
     
     const newCredential = await credentialsRepository.createCredentials({title, url, username, password, userId})
 
     return newCredential;
 }
 
-async function findAllCredentials() {
+async function findAllCredentials() {      
     const credentials = await credentialsRepository.findAllCredentials()
     
     return credentials;
@@ -23,6 +30,18 @@ async function findAllCredentials() {
 
 async function findCredentialById(id: number) {
     const credentials = await credentialsRepository.findCredentialById(id)
+
+   const user = await usersRepository.findUserById(credentials.userId);
+    if(!user) throw {
+        type: "NOT_FOUND",
+        message: "User not found"
+    };
+    
+    if(!credentials) throw {
+        type: "NOT_FOUND",
+        message: "Credential not found"
+    };
+
 
     return credentials;
 }
@@ -35,6 +54,12 @@ async function updateCredential(data: {
   username?: string,
   password?: string
 }) {
+
+    const user = await usersRepository.findUserById(data.userId);
+    if(!user) throw {
+        type: "NOT_FOUND",
+        message: "User not found"
+    };
 
     const conflict = await credentialsRepository.findCredentialByTittle(data.title);
     if (conflict && conflict.userId === data.userId) throw { 
